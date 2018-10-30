46432c49100b95525b1223db4ec0e5d177c9c79b
Shama <shama.hegde@gmail.com>
2018-10-30 18:05:19 +0530

    connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const path=require('path');
-
+const socketIO=require('socket.io');
+const http=require('http');
 const port=process.env.port||3000;
 http.createServer();
 
@@ -11,4 +12,14 @@ const publicName=path.join(__dirname+'/../public');
 var app=express();
 app.use(express.static(publicName));
 
-app.listen(port,()=>console.log("Server is running on 3000"));
+var server=http.createServer(app);
+
+server.listen(port,()=>console.log(`Server running on ${port}`));
+
+var io=socketIO(server);
+io.on('connection',(socket)=>{
+    console.log("client connected!");
+    socket.on('disconnect',()=>{
+                console.log("user was disconnected ");
+            });
+});
